1361d2c1b8007d694be14a5d2f59794f8adbe120
josevabr <josevabram@gmail.com>
2023-03-28 19:31:53 -0700

    git push

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px 
+
+df = pd.read_csv('vehicles_us.csv')
+
+st.header('Distribution of Price in Cars')
+fig = px.histogram(df.price, x = "price", title = "Distribution of Price in Cars")
+st.write(fig)
+
+st.header('Distribution of Models in Cars')
+fig = px.histogram(df, x = 'model', title = 'Distribution of Models in Cars')
+st.write(fig)
+
+st.header("Distribution of Price by Models in Cars")
+fig = px.scatter(df, x = 'price', y = 'model', log_x=[0, 300000], title = 'Distribution of Price by Model of Cars')
+st.write(fig)
+
+st.header("Distribution of Price by Model Years in Cars")
+fig = px.scatter(df, x = 'model_year', y = 'price', log_y=[1, 1000], title = 'Distribution of Price by Model Year in Cars')
+st.write(fig)
+
+check = st.checkbox('This app works!')
+st.write('State of the checkbox:', check)
