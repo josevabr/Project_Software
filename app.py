02458f640818a5dc097274daf1e09d3474248ce4
josevabr <josevabram@gmail.com>
2023-04-04 20:51:23 -0700

    git push

--- app.py
+++ app.py
@@ -2,29 +2,43 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px 
 
-df = pd.read_csv('vehicles_us.csv')
+st.set_page_config(
+    page_title="Project Software", 
+    page_icon="✅",
+    layout='wide',
+)
+@st.cache_data
+def get_data() -> pd.DataFrame:
+    return pd.read_csv('vehicles_us.csv')
 
-
-
-st.header('Distribution of Price in Cars')
-fig = px.histogram(df, x = "price", title = "Distribution of Price in Cars")
-st.write(fig)
-
-st.header('Distribution of Models in Cars')
-fig = px.histogram(df, x = 'model', title = 'Distribution of Models in Cars')
-st.write(fig)
+df = get_data()
 
 st.header("Distribution of Price by Models in Cars")
 fig = px.scatter(df, x = 'price', y = 'model', log_x=[0, 300000], title = 'Distribution of Price by Model of Cars')
 st.write(fig)
 
-model_options = df['model'].unique().tolist()
-model = st.selectbox('Car Model', model_options, 0)
-df = df[df['model']== model]
-
 st.header("Distribution of Price by Model Years in Cars")
 fig = px.scatter(df, x = 'model_year', y = 'price', log_y=[1, 1000], title = 'Distribution of Price by Model Year in Cars')
 st.write(fig)
+
+st.title('Distribution of Price in Car Model and Car Model Years')
+model_filter = st.selectbox('Select Model', pd.unique(df['model']))
+df = df[df['model']== model_filter]
+price_filter = st.selectbox('Select Price', pd.unique(df['price']))
+df = df[df['price']== price_filter]
+year_filter = st.selectbox('Select Model Year', pd.unique(df['model_year']))
+df = df[df['model_year']== year_filter]
+
+fig_col1, fig_col2 = st.columns(2)
+with fig_col1:
+    st.markdown('### Distribution of Price in Cars')
+    fig = px.histogram(df, x = "price")
+    st.write(fig)
+
+with fig_col2:
+    st.markdown('### Distribution of Models in Cars')
+    fig = px.histogram(df, x = 'model')
+    st.write(fig)
 
 check = st.checkbox('This app works!')
 st.write('State of the checkbox:', check)
