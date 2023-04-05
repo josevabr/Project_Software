96fbcd5491b583f2751f70c909395615714090e8
josevabr <josevabram@gmail.com>
2023-04-04 19:56:45 -0700

    git push

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import plotly.express as px
 
 df = pd.read_csv('vehicles_us.csv')
 
-model_options = df['model'].unique().tolist()
-model = st.selectbox('Car Model', model_options, 0)
-df = df[df['model']== model]
+
 
 st.header('Distribution of Price in Cars')
 fig = px.histogram(df, x = "price", title = "Distribution of Price in Cars")
@@ -19,6 +17,10 @@ st.write(fig)
 st.header("Distribution of Price by Models in Cars")
 fig = px.scatter(df, x = 'price', y = 'model', log_x=[0, 300000], title = 'Distribution of Price by Model of Cars')
 st.write(fig)
+
+model_options = df['model'].unique().tolist()
+model = st.selectbox('Car Model', model_options, 0)
+df = df[df['model']== model]
 
 st.header("Distribution of Price by Model Years in Cars")
 fig = px.scatter(df, x = 'model_year', y = 'price', log_y=[1, 1000], title = 'Distribution of Price by Model Year in Cars')
